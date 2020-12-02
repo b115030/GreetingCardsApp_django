93922d0d41defd222978ed8ad1f01503232a4675
b115030 <iitiangokul@gmail.com>
2020-12-02 12:15:22 +0530

    [Gopinath]Added: functionality to read data from backend

--- greeting_messages/views.py
+++ greeting_messages/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Greetings
+from django.views import View
+from django.core import serializers
+from django.http import HttpResponse
 
 # Create your views here.
 
 def get(request):
     post = Greetings.objects.all()
-    return render(request, 'greeting_messages/index.html', {'mydata_json': post})
+    return render(request, 'greeting_messages/index.html', {'myjson': post})
+
+class GreetingView(View):
+    def get(self, request):
+
+        post = Greetings.objects.all()
+        print(post)
+        data = serializers.serialize('json', post)
+        return HttpResponse(data)
